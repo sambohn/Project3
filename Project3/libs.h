4b31d4b72cedb9788f3633f380078110212c4d12
bohns <bohns@AAD-PF4E4PLC>
2024-05-23 18:45:17 -0500

    Add project files.

---
+++ Project3/libs.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+#include <glew.h>
+#include <glfw3.h>
+
+// OpenGL Mathematics Library
+#include <glm.hpp>
+#include <vec2.hpp> // glm::vec2
+#include <vec3.hpp> // glm::vec3
+#include <vec4.hpp> // glm::vec4
+#include <mat4x4.hpp> // glm::mat4
+#include <gtc/matrix_transform.hpp> // glm::translate, glm::rotate, glm::scale, glm::perspective
+#include <gtc\type_ptr.hpp>
+
+// load images & textures
+#include <SOIL2.h>
+
+
---
+++ Project3/main.cpp
@@ -0,0 +1,32 @@
+#pragma once
+#include "libs.h"
+
+
+int main() {
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Window", NULL, NULL);
+    if (!window) {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Render OpenGL here
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
